e062bd4894fd85ebd023554769bebaa53e9a3803
incyberz <incyberz@yahoo.com>
2023-08-28 05:52:08 +0700

    update

---
+++ lat01/src/main.ts
@@ -0,0 +1,20 @@
+class Coder {
+  name: string   
+  music: string   
+  age: number   
+  lang: string  
+  
+  constructor(
+    name:string,
+    music:string,
+    age:number,
+    lang:string,
+    
+  ){
+    this.name = name
+    this.music = music
+    this.age = age
+    this.lang = lang
+  }
+}
+
